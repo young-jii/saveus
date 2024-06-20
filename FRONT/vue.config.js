69b75ac164ac90ef57d15bc59941757ebd959bbc
young-jii <forest66young@gmail.com>
2024-06-20 12:35:07 +0900

    [front] public path 추가

--- FRONT/vue.config.js
+++ FRONT/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/saveus/' : '/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
