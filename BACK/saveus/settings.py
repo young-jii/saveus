e9eac32514d9267866ae2c0d78c2356af2792b26
young-jii <forest66young@gmail.com>
2024-06-20 15:50:07 +0900

    [back] settings.py 허가 코드 추가

--- BACK/saveus/settings.py
+++ BACK/saveus/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = [
     'localhost',
     "https://young-jii.github.io/saveus/",  # 8080 서버
     "3.35.141.132",
+    "ec2-3-35-141-132.ap-northeast-2.compute.amazonaws.com"
     ]
 
 INSTALLED_APPS = [
@@ -124,6 +125,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
     "https://young-jii.github.io",
     "https://3.35.141.132",
+    "https://ec2-3-35-141-132.ap-northeast-2.compute.amazonaws.com"
 ]
 
 # CSRF 설정
@@ -132,6 +134,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8080",
     "https://young-jii.github.io/saveus/",
     "https://3.35.141.132",
+    "https://ec2-3-35-141-132.ap-northeast-2.compute.amazonaws.com"
 ] # Vue.js 개발 서버 주소를 신뢰된 출처로 추가
 
 ROOT_URLCONF = 'saveus.urls'
