ad0e9e6c9c3487388935af096e821b7730fdf4c6
young-jii <forest66young@gmail.com>
2024-06-20 12:24:33 +0900

    [front] vue.config.js 추가

---
+++ FRONT/vue.config.js
@@ -0,0 +1,48 @@
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+      }),
+    ],
+  },
+  devServer: {
+    allowedHosts: [
+      "young-jii.github.io/saveus/",
+      "3.35.141.132",
+      'localhost',
+    ],
+    host: '0.0.0.0', // 외부 접속을 허용
+    port: 8080,
+    https: false, // HTTPS 사용
+    client: {
+      webSocketURL: {
+        protocol: 'wss',
+        hostname: 'young-jii.github.io/saveus/',
+        port: 443,
+        pathname: '/ws',
+      },
+    },
+    proxy: {
+      '/api': {
+        target: 'https://3.35.141.132',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/calculate': {
+        target: 'https://3.35.141.132',
+        changeOrigin: true,
+      },
+    },
+    headers: {
+      'Access-Control-Allow-Origin': 'https://3a145eca76f9.ngrok.app', // 특정 도메인만 허용
+      'Access-Control-Allow-Credentials': 'true', // 자격 증명 허용
+    },
+  },
+});
