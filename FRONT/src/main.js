ee12b665b6e311513c555b8f9de331f8cf504219
young-jii <forest66young@gmail.com>
2024-06-25 23:05:38 +0900

    [front] fix main.js code as vue3

--- FRONT/src/main.js
+++ FRONT/src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios';
-import Vue from 'vue';
+import mitt from 'mitt';
 
-Vue.prototype.$eventBus = new Vue();
+const eventBus = mitt();
 
 // Axios 기본 설정
 axios.defaults.withCredentials = true;
@@ -32,28 +32,6 @@ axios.get(`${apiBaseUrl}/map/set-csrf-token/`, { withCredentials: true })
         console.error('Error getting CSRF Token:', error.response || error.message);
     });
 
-// // WebSocket URL 설정 (HTTPS에 맞춰 WSS 사용)
-// const wsBaseUrl = process.env.VUE_APP_WS_BASE_URL || 'wss://https://jiyoung.pythonanywhere.com';
-
-// if (!wsBaseUrl) {
-//     console.error('WebSocket Base URL is not defined. Check your .env file.');
-// } else {
-//     console.log(`WebSocket Base URL: ${wsBaseUrl}`);
-// }
-
-// // WebSocket 연결 (오류 처리 개선)
-// let socket;
-// try {
-//     socket = new WebSocket(`${wsBaseUrl}/ws/some_path/`);
-
-//     socket.onopen = () => console.log('WebSocket connection opened.');
-//     socket.onmessage = (event) => console.log('WebSocket message received:', event.data);
-//     socket.onclose = () => console.log('WebSocket connection closed.');
-//     socket.onerror = (error) => console.error('WebSocket error:', error);
-// } catch (error) {
-//     console.error('Failed to establish WebSocket connection:', error);
-// }
-
 // ODSAY API 호출용 Axios 인스턴스 생성
 const odsayAxiosInstance = axios.create({
     baseURL: 'https://api.odsay.com/v1/api/',
@@ -64,6 +42,6 @@ const app = createApp(App);
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$odsayAxios = odsayAxiosInstance;
 app.config.globalProperties.$apiBaseUrl = apiBaseUrl;
-// app.config.globalProperties.$socket = socket;
+app.config.globalProperties.$eventBus = eventBus;
 
 app.use(router).mount('#app');
