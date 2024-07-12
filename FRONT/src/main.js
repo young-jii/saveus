8b9e4e3f1fe51a2ffb554b2ff65fcb919fcbd4ae
young-jii <forest66young@gmail.com>
2024-07-13 01:01:09 +0900

    refix37

--- FRONT/src/main.js
+++ FRONT/src/main.js
@@ -5,6 +5,23 @@ import axios from 'axios';
 import EventBus from '../eventBus';
 import store from './store'; // store 파일 import 추가
 
+// CSRF 토큰을 쿠키에서 가져오는 함수
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 // Axios 기본 설정
 axios.defaults.withCredentials = true;
 axios.defaults.xsrfCookieName = 'csrftoken';
@@ -23,7 +40,7 @@ if (!apiBaseUrl) {
 // CSRF 토큰을 가져와 Axios에 설정
 axios.get(`${apiBaseUrl}/map/set-csrf-token/`, { withCredentials: true })
     .then(response => {
-        const csrfToken = response.data.csrfToken;
+        const csrfToken = getCookie('csrftoken');
         console.log('CSRF Token received:', csrfToken);
         axios.defaults.headers.common['X-CSRFToken'] = csrfToken;
     })
