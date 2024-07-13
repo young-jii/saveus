8df1d772008b81fe6edf4a0c6db49be3e48f0dc3
young-jii <forest66young@gmail.com>
2024-07-13 21:08:32 +0900

    refix47

--- FRONT/src/store/index.js
+++ FRONT/src/store/index.js
@@ -3,22 +3,6 @@
 import { createStore } from 'vuex';
 import axios from 'axios';
 
-// Helper function to get CSRF token from cookies
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 const instance = axios.create({
     baseURL: 'https://jiyoung.pythonanywhere.com/',  // Django API의 기본 URL
     withCredentials: true  // 자격 증명을 포함한 요청 허용
