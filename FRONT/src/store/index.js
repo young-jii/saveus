31f44648da42a7cd3eaa9e178a31adbbf3441567
young-jii <forest66young@gmail.com>
2024-07-13 20:32:05 +0900

    refix44

--- FRONT/src/store/index.js
+++ FRONT/src/store/index.js
@@ -1,5 +1,22 @@
 import { createStore } from 'vuex';
 
+// Helper function to get CSRF token from cookies
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Check if the cookie starts with the name we are looking for
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 const store = createStore({
     state: {
         selectedRoute: null,
