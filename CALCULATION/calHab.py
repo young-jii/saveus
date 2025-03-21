d95ce33be342143633ef26f6a9ee65801f95456f
Choi-Daye <allyes1227@gmail.com>
2025-03-21 17:50:04 +0900

    change comments

--- CALCULATION/calHab.py
+++ CALCULATION/calHab.py
@@ -1,11 +1,12 @@
-id = "최다예"
+# 코드 통합본
+id = "다예"
 home = "서울"
 young = "N"
 subsidiary = "N"
 payment = 1700
 start_point = "경복궁역2번출구"
 end_point = "총신대입구역.남성사계시장입구앞"
-transport = ["7016","502"] # 버스일 때만 집어넣기 -> 지하철이면 복잡해짐
+transport = ["7016","502"] 
 # 이동 횟수 : 4~5주 (20~25일 -> 40~50회)
 # min : 40, max : 50
 
